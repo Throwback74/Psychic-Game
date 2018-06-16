d01dede31d645a56dfb95e63e1e0b213b68ddd8c
Throwback74 <corey.slade@gmail.com>
2018-06-16 01:41:17 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,8 @@ var userLosses = 0;
 var userGuesses = 10;
 // userChoices is a currently empty array that will be filled with the user's guesses as the rounds proceed
 var userChoices = [];
-// var letter = null;
+var letterGuessed = null;
 // for(var uC = 0; uC <= userGuesses;  )
-
-var targetDiv = document.getElementById("guessText").innerHTML = userChoices;
-
 // document.getElementById('show_btn').onclick = function(e) {
 //     alert(record);
 // }
@@ -19,35 +16,43 @@ var chars = [
 //have the computer choose a random number betwee 1 and 26 and convert it into a string
 var compLtr = chars[Math.floor(Math.random() * chars.length)];
 console.log("Wins: " + userScore + "Losses: " + userLosses + "Guesses Left: " + userGuesses);
+
+var targetGuessT = $("#guessText").html();
+
+var updateUserGuesses = function() {
+    $("#guessesLeft").html() = "Guesses Left: " + userGuesses;
+};
+
+
 // when the user presses a letter on the keyboard...
 document.onkeyup = function(event) {
 
-    var letter = (event.key).toLowerCase();
+    // var myFunction = function(array) {
 
-    Array.prototype.push.apply(userChoices, letter);
+    //     for(var i = 0; i < array.length; i++) {
+    //         console.log(array[i]);
+    //     }
+    
+    var letter = [String.fromCharCode((event.key).toLowerCase())];
 
-    console.log(userChoices);
+    var guessesMade = Array.prototype.push.apply(userChoices, letter);
 
-       var guessArray = function() {
+    console.log(guessesMade);
 
-            userChoices.push(letter);
+    // myFunction(userChoices);
+
+    //    var guessArray = function() {
+
+    //         userChoices.push(letter);
             
-            document.getElementById("guessText").innerHTML = function(){
+    //         document.getElementById("guessText").innerHTML = function(){
             
-            console.log(userChoices)
+    //         console.log(userChoices)
 
-            guessArray(letter);
-            };    
-        };
-
-    function myFunction(array) {
-
-        for(var i = 0; i < array.length; i++) {
-            document.write(array[i]);
-        }
-    }
-
-myFunction(userChoices);
+    //         guessArray(letter);
+    //         };    
+    //     };
+}
 //make a function to reset the picker, up the score or losses and reset the guesses left once the user wins/loses
 
 //make a function that adds each letter guessed to the empty array - userChoices
@@ -61,6 +66,6 @@ myFunction(userChoices);
 
     // };
 
-}
+
 
     // if(letter === )
