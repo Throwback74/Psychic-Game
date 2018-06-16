f421c5dfabe68af6b495a2a9175288092e25350a
Throwback74 <corey.slade@gmail.com>
2018-06-16 06:12:17 -0700

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
+$( document ).ready(function() {
 // set the score, losses and guesses left to 0
 var userScore = 0;
 var userLosses = 0;
 var userGuesses = 10;
 // userChoices is a currently empty array that will be filled with the user's guesses as the rounds proceed
 var userChoices = [];
-var letterGuessed = null;
+var letterNeeded = null;
 // for(var uC = 0; uC <= userGuesses;  )
 // document.getElementById('show_btn').onclick = function(e) {
 //     alert(record);
@@ -15,29 +16,70 @@ var chars = [
         ];
 //have the computer choose a random number betwee 1 and 26 and convert it into a string
 var compLtr = chars[Math.floor(Math.random() * chars.length)];
-console.log("Wins: " + userScore + "Losses: " + userLosses + "Guesses Left: " + userGuesses);
+    console.log("Wins: " + userScore + "Losses: " + userLosses + "Guesses Left: " + userGuesses);
 
-var targetGuessT = $("#guessText").html();
+// var targetGuessT = $("#guessText").html();
+
+var updateGuessesRemain = function() {
+     $("#guessesLeft").html("Guesses Left: " + userGuesses);
+    };
+    //Not sure if this is right, need to check it - see if we need a .this
+var updateLetterNeeded = function() {
+     $(compLtr) = $(letterNeeded);
+    };
 
 var updateUserGuesses = function() {
-    $("#guessesLeft").html() = "Guesses Left: " + userGuesses;
-};
+    $("#guessText").html($(userChoices.join(', '))); 
+    // $(userChoices).join(', ')
+    //  $("#guessText").html(userChoices.join(", "));
+    }
 
+    var reset = function() {
+        userGuesses = 10;
+        userChoices = [];
 
-// when the user presses a letter on the keyboard...
-document.onkeyup = function(event) {
+        updateGuessesRemain();
+        updateLetterNeeded();
+        updateUserGuesses();
+    }
+
+    updateLetterNeeded();
+    // when the user presses a letter on the keyboard...
+    document.onkeyup = function(event) {
+
+        var letter = String.fromCharCode(event.key).toLowerCase();
+        userGuesses--;
+
+        userChoices.push(letter);
+        updateGuessesRemain();
+        updateUserGuesses();
+
+            if(userGuesses > 0) {
+                if(letter == letterNeeded) {
+                    userScore++;
+                    $("#score").html("Wins: " + userScore);
+                    $("#profX").attr("src", "assets/images/GoodJob.jpg");
+                    reset();
+                }
+            }else if(userGuesses == 0) {
+                userLosses++;
+                $("#losses").html("Losses: " + userLosses);
+                $("#profX").attr("src", "assets/images/Sorry.jpg");
+                reset();
+            }
 
     // var myFunction = function(array) {
+//To make the image invisible - // $("#profX").style.display= "none";
 
     //     for(var i = 0; i < array.length; i++) {
     //         console.log(array[i]);
     //     }
     
-    var letter = [String.fromCharCode((event.key).toLowerCase())];
+    
 
-    var guessesMade = Array.prototype.push.apply(userChoices, letter);
+    // var guessesMade = Array.prototype.push.apply(userChoices, letter);
 
-    console.log(guessesMade);
+    // console.log(guessesMade);
 
     // myFunction(userChoices);
 
@@ -69,3 +111,4 @@ document.onkeyup = function(event) {
 
 
     // if(letter === )
+})
